c36b8a5822cee3c83841c16126276acc89a1ba8f
akshayah3 <akshaynukala95@gmail.com>
2014-03-29 01:06:03 +0530

    some minor changes

--- Python/Linear Regression/regression.py
+++ Python/Linear Regression/regression.py
@@ -10,14 +10,15 @@ Linear regression
 """
 
 import numpy as np
+import matplotlib as mp
 
 def data(filename):
     datas = np.loadtxt(str(filename))
-    X = zeros([len(y), 1])
+    X = np.zeros([len(y), 1])
     X[:, 1] = datas[:, 0]
     y = datas[:, 1]
-    theta = zeros([len(y),2])
-    theta[:,0] = ones(len(y))
+    theta = np.zeros([len(y),2])
+    theta[:,0] = np.ones(len(y))
     return X, y , theta    
     
 def cost_function(X, y, theta):
@@ -29,6 +30,16 @@ def cost_function(X, y, theta):
     return cost
 
 def gradient_descent(X, y, theta, alpha, iters):
+    grad = np.zeros([len(iters), 1])
     for i in range(iters):
         for j in range(len(y)):
-                        
+            theta[1][j] = theta[1][j] - (alpha/len(y))*(y[j] - X.T[j].dot(theta[j]))*X[1][j]
+        grad[i] = cost_function(X, y , theta)
+    return grad
+                
+def predict(X):
+        
+    
+    
+    
+    
